da67a0e09bf6c4514d0e3235704cf0caf049f59a
Tymonello <tymoteusz1138@gmail.com>
2023-05-30 22:24:40 +0200

    Created empty interfaces for Services

---
+++ Popkultulista.Application/Interfaces/IItemService.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="IItemService.cs" company="tymonello">
+// Copyright (c) tymonello. All rights reserved.
+// </copyright>
+
+namespace Popkultulista.Application.Interfaces;
+
+using Popkultulista.Domain.Models;
+
+/// <summary>
+/// An interface for <see cref="FomoItem"/> service.
+/// </summary>
+public interface IItemService
+{
+}
---
+++ Popkultulista.Application/Interfaces/IListService.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="IListService.cs" company="tymonello">
+// Copyright (c) tymonello. All rights reserved.
+// </copyright>
+
+namespace Popkultulista.Application.Interfaces;
+
+using Popkultulista.Domain.Models;
+
+/// <summary>
+/// An interface for <see cref="FomoItem"/> service.
+/// </summary>
+public interface IListService
+{
+}
